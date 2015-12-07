298c81be839662b11d302c836a404e393cffcdfa
Elias Ruemmler <git@rc-art.de>
2015-12-08 00:13:10 +0100

    Update settings.php

--- templates/settings.php
+++ templates/settings.php
@@ -1,10 +1,10 @@
 <div class="wrap">
-    <h2>WP Plugin Template</h2>
+    <h2>Entry List</h2>
     <form method="post" action="options.php"> 
-        <?php @settings_fields('wp_plugin_template-group'); ?>
-        <?php @do_settings_fields('wp_plugin_template-group'); ?>
+        <?php @settings_fields('rca_entry_list-group'); ?>
+        <?php @do_settings_fields('rca_entry_list-group'); ?>
 
-        <?php do_settings_sections('wp_plugin_template'); ?>
+        <?php do_settings_sections('rca_entry_list'); ?>
 
         <?php @submit_button(); ?>
     </form>
